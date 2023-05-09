69bc16e8ff8bd3de24f9a5a3291f9fc81c876e79
dipesh162 <dipeshsingh162@gmail.com>
2023-05-09 12:29:46 +0530

    FIxed indentation

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,6 +1,6 @@
+import React from "react";
 import { faArrowRightLong, faEarthAmericas, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 
 
 export default function Footer(){
