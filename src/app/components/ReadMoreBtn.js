0bfe419734343ade7598ea785e38f84392f75e3b
dipesh162 <dipeshsingh162@gmail.com>
2023-05-17 17:33:11 +0530

    ADded link in overlay dr's intro

--- src/app/components/ReadMoreBtn.js
+++ src/app/components/ReadMoreBtn.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function ReadMoreBtn(){
     return(
-        <div className='pt-2.5 pb-5'>
-            <a className='mx-auto text-[#999999] rounded-[3px] text-[13px] bg-white border-2 border-white py-[8px] px-[43px]'>
+        <div className="pt-2.5 pb-5">
+            <a href='/doctor/dr-birinder-singh-thind/' className='mx-auto text-[#999999] hover:text-[#13c5dd] hover:cursor-pointer rounded-[3px] text-[13px] bg-white border-2 border-white py-[8px] px-[43px]'>
                 Read More
             </a>
         </div>
