8b332ba335e4bce8d07ddfc3c9d94053f5c02d5f
dipesh162 <dipeshsingh162@gmail.com>
2023-05-17 12:59:40 +0530

    FIxed Testimonials container width

--- src/app/components/HomePage/Testimonials.js
+++ src/app/components/HomePage/Testimonials.js
@@ -5,19 +5,21 @@ import TestimonialsProofs from './TestimonialsProofs';
 export default function Testimonials(){
     return(
 
-        <div className="bg-cover bg-no-repeat bg-center box-border pt-[40px] lg:pt-[70px] pb-[35px] lg:pb-[55px] px-2.5 md:px-16 grid grid-cols-1 md:grid-cols-2 lg:md:grid-cols-4"
+        <div className="bg-cover bg-no-repeat bg-center box-border pt-[40px] lg:pt-[70px] pb-[35px] lg:pb-[55px] px-2.5 md:px-16"
             style={{
                 backgroundImage: `url("/images/Home/banner-slide-two.png")`,
             }}
         >
-            {TestimonialsData.map((testimonial,i)=>(
-                <TestimonialsProofs
-                    key={i}
-                    number={testimonial.number}
-                    item={testimonial.item}
-                />
-            ))
-            }
+            <div className='max-w-[1170px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:md:grid-cols-4'>
+                {TestimonialsData.map((testimonial,i)=>(
+                    <TestimonialsProofs
+                        key={i}
+                        number={testimonial.number}
+                        item={testimonial.item}
+                    />
+                ))
+                }
+            </div>
         </div>
     )
 }
