d5611a89ed2bc312d5b87e0e999fb4252d877ad0
dipesh162 <dipeshsingh162@gmail.com>
2023-05-17 17:32:16 +0530

    Added counter in testimonials

--- src/app/components/HomePage/TestimonialsProofs.js
+++ src/app/components/HomePage/TestimonialsProofs.js
@@ -1,9 +1,15 @@
 import React from "react";
+import CountUp from 'react-countup';
 
 export default function TestimonialsProofs({number, item}) {
     return(
         <div className="text-white m-auto pb-6 md:pb-[50px] text-center font-sans pt-9 px-6">
-            <h2 className="text-[38px] md:text-[55px] pb-2.5 leading-[35px]">{number}</h2>
+            <h2 className="text-[38px] md:text-[55px] pb-2.5 leading-[35px]">
+                <CountUp
+                    end={number} 
+                    duration={2}   
+                />
+            </h2>
             <h5 className="text-[18px] pb-2.5 leading-[1.2]">{item}</h5>
         </div>
     )
